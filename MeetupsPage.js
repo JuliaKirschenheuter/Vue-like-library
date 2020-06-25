251209b71bc5430148f1e02c7a1b96c6fcb8bfb3
Julia Kirschenheuter <Julia.Kirschenheuter@gmail.com>
2020-06-25 13:47:42 +0200

    part-2: PageTabs were done as Component

--- MeetupsPage.js
+++ MeetupsPage.js
@@ -1,6 +1,7 @@
 import meetups from "./data/meetups-data.js";
 import {MeetupsList} from "./MeetupsList.js";
 import {MeetupsCalendar} from "./MeetupsCalendar.js";
+import {PageTabs} from "./PageTabs.js";
 
 const template = `
     <div class="container">
@@ -54,45 +55,15 @@ const template = `
                     </div>
                 </div>
                 <div class="form-group form-group_inline">
-          <span>
-            <div class="page-tabs">
-              <button type="button"
-                      class="page-tabs__tab"
-                      :class="{ 'page-tabs__tab_active': filter.view === '' || filter.view === 'list'  }"
-                      @click="filter.view = 'list' "
-              >
-                <svg fill="none" height="28" viewBox="0 0 28 28" width="28" xmlns="http://www.w3.org/2000/svg">
-                  <path clip-rule="evenodd"
-                        d="m5 7c-.55228 0-1 .44772-1 1s.44772 1 1 1h.01c.55228 0 1-.44772 1-1s-.44772-1-1-1zm5 0c-.55228 0-1 .44772-1 1s.44772 1 1 1h13c.5523 0 1-.44772 1-1s-.4477-1-1-1zm0 6c-.55228 0-1 .4477-1 1s.44772 1 1 1h13c.5523 0 1-.4477 1-1s-.4477-1-1-1zm-1 7c0-.5523.44772-1 1-1h13c.5523 0 1 .4477 1 1s-.4477 1-1 1h-13c-.55228 0-1-.4477-1-1zm-5-6c0-.5523.44772-1 1-1h.01c.55228 0 1 .4477 1 1s-.44772 1-1 1h-.01c-.55228 0-1-.4477-1-1zm1 5c-.55228 0-1 .4477-1 1s.44772 1 1 1h.01c.55228 0 1-.4477 1-1s-.44772-1-1-1z"
-                        fill-rule="evenodd"></path>
-                </svg>
-              </button>
-              <button type="button" class="page-tabs__tab"
-                      @click="filter.view = 'calendar' "
-                      :class="{ 'page-tabs__tab_active': filter.view === 'calendar'  }"
-              >
-                <svg height="22" viewBox="0 0 20 22" width="20" xmlns="http://www.w3.org/2000/svg">
-                  <path clip-rule="evenodd"
-                        d="m15 1c0-.552285-.4477-1-1-1s-1 .447715-1 1v1h-6v-1c0-.552285-.44772-1-1-1s-1 .447715-1 1v1h-2c-1.65685 0-3 1.34315-3 3v14c0 1.6569 1.34315 3 3 3h14c1.6569 0 3-1.3431 3-3v-14c0-1.65685-1.3431-3-3-3h-2zm3 7v-3c0-.55228-.4477-1-1-1h-2v1c0 .55228-.4477 1-1 1s-1-.44772-1-1v-1h-6v1c0 .55228-.44772 1-1 1s-1-.44772-1-1v-1h-2c-.55228 0-1 .44772-1 1v3zm-16 2h16v9c0 .5523-.4477 1-1 1h-14c-.55228 0-1-.4477-1-1z"
-                        fill-rule="evenodd"></path>
-                </svg>
-              </button>
-            </div>
-          </span>
+                    <page-tabs :selected="filter.view" @change="filter.view=$event"></page-tabs>
                 </div>
             </div>
         </div>
-
-        <template v-if="filteredMeetups && filteredMeetups.length">
-            <transition name="fade" mode="out-in">
-                <meetups-list key="list" v-if="filter.view === '' || filter.view === 'list' "
-                :meetups="filteredMeetups"></meetups-list>
-                <meetups-calendar v-else-if="filter.view === 'calendar' " 
-                :key="calendar"
-                :meetup="filteredMeetups"></meetups-calendar>
-            </transition>
-
-        </template>
+          
+        <transition v-if="filteredMeetups && filteredMeetups.length" name="fade" mode="out-in">
+            <meetups-list v-if="filter.view === '' || filter.view === 'list'" :meetups="filteredMeetups" key="list"></meetups-list>
+            <meetups-calendar v-else-if="filter.view === 'calendar'" key="calendar" :meetups="filteredMeetups" ></meetups-calendar>
+        </transition>
 
         <div v-else class="meetup__empty">Митапов по заданным условям не найдено...</div>
     </div>
@@ -102,7 +73,8 @@ export const MeetupsPage = {
     template,
     components: {
         MeetupsList,
-        MeetupsCalendar
+        MeetupsCalendar,
+        PageTabs
     },
 
     data() {
